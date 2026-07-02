d601b477dc2e2dd1da0b9d1da16a78a77780a3ce
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-07-02 22:53:48 +0800

    创建 EpubMimetype.java

---
+++ src/cn/imaginary/toolkit/document/epub/EpubMimetype.java
@@ -0,0 +1,11 @@
+package cn.imaginary.toolkit.document.epub;
+
+import cn.imaginary.toolkit.document.epub.mimetype.EpubMimetypesFileTypeMap;
+
+public class EpubMimetype {
+    private String tag = EpubMimetypesFileTypeMap.Mimetype_Epub;
+
+    public String toString() {
+        return tag;
+    }
+}
