f17ffa310ed4be06f45252aa8f9ff86dbbf00dd5
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-07-05 22:04:45 +0800

    更新 EpubMimetype.java

--- src/cn/imaginary/toolkit/document/epub/EpubMimetype.java
+++ src/cn/imaginary/toolkit/document/epub/EpubMimetype.java
@@ -3,9 +3,5 @@ package cn.imaginary.toolkit.document.epub;
 import cn.imaginary.toolkit.document.epub.mimetype.EpubMimetypesFileTypeMap;
 
 public class EpubMimetype {
-    private String tag = EpubMimetypesFileTypeMap.Mimetype_Epub;
-
-    public String toString() {
-        return tag;
-    }
+    public static String Media_Type = EpubMimetypesFileTypeMap.Mimetype_Epub;
 }
