3e406758c27b8ad40406c3b93afc95f502adb7bc
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-07-05 22:05:34 +0800

    创建 HtmlCover.java

---
+++ src/cn/imaginary/toolkit/document/epub/HtmlCover.java
@@ -0,0 +1,17 @@
+package cn.imaginary.toolkit.document.epub;
+
+public class HtmlCover {
+    public static String Uri = "http://www.w3.org/1999/xhtml";
+    public static String Sign = "html";
+    public static String Sign_Head = "head";
+    public static String Sign_Head_Title = "title";
+    public static String Sign_Head_Style = "style";
+    public static String Sign_Body = "body";
+    public static String Sign_Body_Table = "table";
+    public static String Sign_Body_Table_Image = "img";
+    public static String Name_Head_Style_Type = "type";
+    public static String Name_Body_Table_Image_Src = "src";
+    public static String Name_Body_Table_Image_Alt = "alt";
+    public static String Value_Head_Title = "Cover";
+    public static String Value_Head_Style = "html, body .table {height: 100%; width:100%; text-align:center;}";
+}
