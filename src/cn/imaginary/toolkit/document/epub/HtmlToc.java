78678c99d67f6f31c2bf0f96e4f83cc9eab88217
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-07-05 22:06:06 +0800

    更新 HtmlToc.java

--- src/cn/imaginary/toolkit/document/epub/HtmlToc.java
+++ src/cn/imaginary/toolkit/document/epub/HtmlToc.java
@@ -1,4 +1,26 @@
 package cn.imaginary.toolkit.document.epub;
 
 public class HtmlToc {
+    public static String Uri_Epub = "http://www.idpf.org/2007/ops";
+    public static String Sign_Prefix = "!DOCTYPE html";
+    public static String Sign_Prefix_Type = "!DOCTYPE";
+    public static String Uri = "http://www.w3.org/1999/xhtml";
+    public static String Sign = "html";
+    public static String Sign_Head = "head";
+    public static String Sign_Head_Title = "title";
+    public static String Sign_Body = "body";
+    public static String Sign_Body_Nav = "nav";
+    public static String Sign_Body_Nav_H1 = "h1";
+    public static String Sign_Body_Nav_OL = "ol";
+    public static String Sign_Body_Nav_LI = "li";
+    public static String Sign_Body_Nav_A = "a";
+    public static String Name_Epub = "epub";
+    public static String Name_Body_Nav_Data_Type = "type";
+    public static String Name_Body_Nav_ID = "id";
+    public static String Name_Body_Nav_A_Href = "href";
+    public static String Value_Head_Title = "Table Of Content";
+    public static String Value_Body_Nav_Type = "toc";
+    public static String Value_Body_Nav_H1 = "Content";
+    public static String Value_Body_Nav_Data_Type = Value_Body_Nav_Type;
+    public static String Value_Body_Nav_ID = Value_Body_Nav_Type;
 }
