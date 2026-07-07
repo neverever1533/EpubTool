2abf57bdab8b11bc39f73226250c9db7bbc7465d
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-07-07 21:56:46 +0800

    Add ZipTool class for zip file operations
    Created a new ZipTool class for handling zip operations.

---
+++ src/javaev/util/ZipTool.java
@@ -0,0 +1,15 @@
+package javaev.util;
+
+import javaev.io.FileTool;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.nio.file.Files;
+
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipTool {
+}
