0c1f1b16cb38b8a68090dcd2218e7454d821681c
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-07-08 13:02:32 +0800

    更新 ZipTool.java

--- src/javaev/util/ZipTool.java
+++ src/javaev/util/ZipTool.java
@@ -1,7 +1,5 @@
 package javaev.util;
 
-import javaev.io.FileTool;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,18 +10,69 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipTool {
-    private String name_Zip_Entry_Root;
     private String suffix_zip = ".zip";
     private String tag_slash = "/";
 
-    public void setRootZipEntryName(String name) {
-        name_Zip_Entry_Root = name;
+    public void compressed(File[] array, File zipFile, boolean isPackDirectory) {
+        if (null != array) {
+            File file = array[0];
+            File file_parent = file.getParentFile();
+            if (null == zipFile) {
+                zipFile = new File(file_parent, file_parent.getName() + suffix_zip);
+            }
+            try {
+                ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile.toPath()));
+                String zipEntryName;
+                if (isPackDirectory) {
+                    zipEntryName = file_parent.getName();
+                } else {
+                    zipEntryName = null;
+                }
+                write(array, zipEntryName, zipOutputStream);
+                zipOutputStream.finish();
+                zipOutputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public String getRootZipEntryName() {
-        return name_Zip_Entry_Root;
+    private void write(File file, String zipEntryName, ZipOutputStream zipOutputStream) throws IOException {
+        if (null != file && null != zipOutputStream) {
+            if (file.isDirectory()) {
+                File[] array = file.listFiles();
+                write(array, zipEntryName, zipOutputStream);
+            } else {
+                ZipEntry zipEntry = new ZipEntry(zipEntryName);
+                zipOutputStream.putNextEntry(zipEntry);
+                write(file, zipOutputStream);
+            }
+        }
     }
 
-    public void write(File file, File zipFile) {
+    private void write(File file, ZipOutputStream zipOutputStream) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] arr = new byte[1024];
+        int length;
+        while ((length = fileInputStream.read(arr)) != -1) {
+            zipOutputStream.write(arr, 0, length);
+        }
+        fileInputStream.close();
+    }
+
+    private void write(File[] array, String zipEntryName, ZipOutputStream zipOutputStream) throws IOException {
+        if (null != array && null != zipOutputStream) {
+            for (int i = 0; i < array.length; i++) {
+                File file = array[i];
+                String name = file.getName();
+                String entryName;
+                if (null == zipEntryName) {
+                    entryName = name;
+                } else {
+                    entryName = zipEntryName + tag_slash + name;
+                }
+                write(file, entryName, zipOutputStream);
+            }
+        }
     }
 }
